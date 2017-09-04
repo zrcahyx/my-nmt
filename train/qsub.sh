4bd287e972f37452228182c93b79bd24406c9b24
zrcahyx <zrcensta.zhang@aispeech.com>
2017-09-04 17:04:25 +0800

    add qsub.sh tp tun on server

---
+++ train/qsub.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+function print_help()
+{
+    if [[ 1 == $# ]]; then
+        echo -e "\n\t\033[31m$1\033[0m"
+    fi
+
+    local usage="
+    Usage: \n
+    ./run.sh -h <host_name>
+
+    host_name:
+        guangzhou, wuhan, nanjing
+
+    log file: train.log
+
+    "
+    echo -e "\033[33m${usage}\033[0m"
+}
+
+HOST="guangzhou"
+
+if [ $# -eq 1 ]; then
+    print_help
+    exit 0
+fi
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -h|--hostname)
+    HOST="$2"
+    shift # past argument
+    ;;
+esac
+shift # past argument or value
+done
+
+echo "#!/bin/bash">run.sh
+echo "source /aifs/users/xyc43/env/bin/activate">>run.sh
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64">>run.sh
+echo "python train.py">>run.sh
+
+# ROOT_DIR=`cd .. && pwd`
+qsub -cwd -S /bin/bash -o train.log -j y -l hostname=$HOST -v PYTHONPATH=$ROOT_DIR run.sh
+
+rm run.sh
